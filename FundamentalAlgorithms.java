a73de0cd23252f7fb625b7ba096e756884d677b6
Nguyen Hoang Linh <linh072217@gmail.com>
2021-09-02 20:35:51 +0700

    write convert number to text

--- FundamentalAlgorithms.java
+++ FundamentalAlgorithms.java
@@ -1,4 +1,8 @@
-import sun.tools.java.AmbiguousMember;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FundamentalAlgorithms {
     public static String amendTheSentence(String s) {
@@ -113,7 +117,70 @@ public class FundamentalAlgorithms {
         }
         return result;
     }
-
+    public static String numberToText(int n) {
+		int size = String.valueOf(n).trim().length();
+		byte[] numberArray = new byte[size];
+		int index = 0;
+		int copyN = n;
+		while(copyN > 0) {
+			byte a = (byte)(copyN % 10);
+			copyN /= 10;
+			numberArray[index] = a;
+			index ++;
+		}
+		for (int i = 0; i < size/2; i++) {
+			byte temp = numberArray[i];
+			numberArray[i] = numberArray[size - 1 - i];
+			numberArray[size - 1 - i] = temp;
+		}
+		String[] texts = {"khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin"};
+		String[] units = {"ty", "tram", "muoi", "trieu", "tram", "muoi", "ngan", "tram", "muoi", ""};
+		int lengthOfTexts = texts.length;
+		String result = "";
+		String[] numberTexts = new String[size];
+		for(int i = 0;i < size;i++) {
+			numberTexts[i] = texts[numberArray[i]]; 
+		}
+		//remove tail zero
+		for(int i = size - 1, j = 0; i >= 0; i--) {
+			if(numberArray[i] == 0) {
+				numberTexts[i] = "";
+				units[lengthOfTexts-1-j] = "";
+				j++;
+			}
+			else {
+				break;
+			}
+		}
+		// remove 10 000 000 
+		for(int i = 0; i < size;i++) {	
+			result += numberTexts[i] + " " + units[10 - size + i] +" ";
+		}	
+		List listNumbers =List.of(0,1,2,3,4,5,6,7,8,9);
+		if(size == 5 && (listNumbers.indexOf((float)n/10000) != -1)) {
+			result += "ngan";
+		}
+		else
+			if(size == 6 &&(listNumbers.indexOf((float)n/100000) != -1)) {
+				result += "ngan";
+			}
+			else
+				if(size == 8 && (listNumbers.indexOf((float)n/10000000) != -1)) {
+					result += "trieu";
+				}
+				else
+					if(size == 9 && (listNumbers.indexOf((float)n/100000000) != -1)) {
+						result += "trieu";
+					}
+		result = result.replace("mot muoi", "muoi");
+		Pattern pattern = Pattern.compile("\\w+");
+		Matcher matcher = pattern.matcher(result);
+		result = "";
+		while(matcher.find()) {
+			result += matcher.group() +" ";
+		}
+		return result;
+    }
     public static void main(String[] args) {
         // System.out.println(amendTheSentence("CodesignalIsAwesome"));
         // System.out.println(checkPalindrome("acbca"));
@@ -121,8 +188,8 @@ public class FundamentalAlgorithms {
         // System.out.println(isTandemRepeat("tandemtandem"));
         // System.out.println(truncateString("312248"));
         // System.out.println(
-        //         new FundamentalAlgorithms().stringsCrossover(new String[] { "abc", "aaa", "aba", "bab" }, "bbb"));
-        System.out.println(lineEncoding("aabbbc"));
-
+        // new FundamentalAlgorithms().stringsCrossover(new String[] { "abc", "aaa", "aba", "bab" }, "bbb"));
+        // System.out.println(lineEncoding("aabbbc"));
+    	System.out.println(numberToText(Integer.parseInt(args[0])));
     }
 }
